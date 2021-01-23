3f7e6321aeebfa6ebd4fe2052ceb093d85695420
roboblazers7617 <roboblazers@universityhighschool.org>
2021-01-23 16:58:10 -0500

    Finished control enumeration and adjusted controls.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,9 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+import java.lang.System;
 
 //the A button is
 
@@ -18,10 +21,14 @@ public class Robot extends TimedRobot{
 	private static final int B_BUTTON = 2;
 	private static final int X_BUTTON = 3;
 	private static final int Y_BUTTON = 4;
-	private static final int LEFT_JOYSTICK_VERTICAL = 0;
-	private static final int LEFT_JOYSTICK_HORIZONTAL = 1;
+	private static final int LEFT_BUMPER = 5;
+	private static final int RIGHT_BUMPER = 6;
+	private static final int LEFT_JOYSTICK_HORIZONTAL = 0;
+	private static final int LEFT_JOYSTICK_VERTICAL = 1;
 	private static final int LEFT_TRIGGER = 2;
 	private static final int RIGHT_TRIGGER = 3;
+	private static final int RIGHT_JOYSTICK_HORIZONTAL = 4;
+	private static final int RIGHT_JOYSTICK_VERTICAL = 5;
 
 	private Joystick stick1 = new Joystick(0);
 	private Joystick stick2 = new Joystick(1);
@@ -49,6 +56,10 @@ public class Robot extends TimedRobot{
 	Encoder leftenc;
 	Encoder rightenc;
 	boolean firstTime;
+	//if controls are flipped, 1
+	int flipped = -1;
+
+	AnalogInput testinput = new AnalogInput(0);
 	
 
   @Override
@@ -82,38 +93,43 @@ public class Robot extends TimedRobot{
 
   @Override
   public void teleopPeriodic(){
+//inverse sine scaling for acceleration
+	if (stick1.getRawButton(B_BUTTON)){
+		flipped = -flipped;
+	}
+
 	  double x = stick1.getRawAxis(LEFT_JOYSTICK_HORIZONTAL) * -1;
 	  double y = stick1.getRawAxis(LEFT_JOYSTICK_VERTICAL);
 	  //right trigger rotates clockwise, left trigger rotates counterclockwise. if both are depressed, no rotation
-	  double z = (stick1.getRawAxis(LEFT_TRIGGER) > 0 && stick1.getRawAxis(RIGHT_TRIGGER) > 0) ? 0.0 : stick1.getRawAxis(LEFT_TRIGGER) * .5 + stick1.getRawAxis(RIGHT_TRIGGER) * -5;
+	  double z = (stick1.getRawAxis(LEFT_TRIGGER) > 0 && stick1.getRawAxis(RIGHT_TRIGGER) > 0) ? 0.0 : stick1.getRawAxis(LEFT_TRIGGER) * .5 + stick1.getRawAxis(RIGHT_TRIGGER) * -.5;
 
 	  m_drive.driveCartesian(x, y, z, 0.0);
 
-	  if (stick1.getRawButton(5)){
+	  if (stick1.getRawButton(LEFT_BUMPER)){
 		  intake.set(.5);
 	  }
-	  else if (stick1.getRawButton(6)){
+	  else if (stick1.getRawButton(RIGHT_BUMPER)){
 		  intake.set(-.5);
 	  }
 	  else{
 		  intake.set(0);
 	  }
 
-	  if (stick2.getRawButton(5)){
+	  if (stick2.getRawButton(LEFT_BUMPER)){
 		  tower.set(.3);
 	  }
-	  else if (stick2.getRawAxis(2)>0){
+	  else if (stick2.getRawAxis(LEFT_TRIGGER) > 0){
 		  tower.set(-.3);
 	  }
 	  else{
 		  tower.set(0);
 	  }
 
-    if (stick2.getRawButton(6)) {
+    if (stick2.getRawButton(RIGHT_BUMPER)) {
 		  shooter.set(-.55);
 		  shooterLeft.set(.55);
 	  }
-	  else if (stick2.getRawAxis(3) > 0) {
+	  else if (stick2.getRawAxis(RIGHT_TRIGGER) > 0) {
 			shooter.set(.55);
 			shooterLeft.set(-.55);
 	  }
@@ -123,14 +139,16 @@ public class Robot extends TimedRobot{
 	  }
 
 	  if (stick2.getRawButton(A_BUTTON)){
-		  hopper.set(.3);
+		  hopper.set(.5);
 	  }
 	  else if (stick2.getRawButton(X_BUTTON)){
-		  hopper.set(-.3);
+		  hopper.set(-.5);
 	  }
 	  else{
 	      hopper.set(0);
 	  }
+
+	  System.out.println(testinput.getValue());
 
 	//   if (stick2.getRawButton(2)){
 	// 	  climb.set(.7);
